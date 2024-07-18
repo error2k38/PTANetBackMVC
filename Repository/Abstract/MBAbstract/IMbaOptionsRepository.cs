ee427c06967c842aa102894e10ce3656931897cb
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 22:54:14 +0200

    Se agregan las Interfaces de las operaciones sobre las entidades manejas en el dominio de la solución.

---
+++ Repository/Abstract/MBAbstract/IMbaOptionsRepository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Entities;
+
+namespace Repository.Abstract.MBAbstract
+{
+    /// <summary>
+    /// Defines the interface for the MbaOptions repository.
+    /// </summary>
+    public interface IMbaOptionsRepository : IRepository
+    {
+        /// <summary>
+        /// Asynchronously creates a new <see cref="MbaOptions"/> entity with the specified country and country code.
+        /// </summary>
+        /// <param name="country">The country for the new <see cref="MbaOptions"/> entity.</param>
+        /// <param name="countryCode">The country code for the new <see cref="MbaOptions"/> entity.</param>
+        /// <returns>The newly created <see cref="MbaOptions"/> entity.</returns>
+        Task<MbaOptions> CreateMbaOptionsAsync(string country, string countryCode);
+
+        /// <summary>
+        /// Asynchronously creates a new <see cref="MbaOptions"/> entity with the specified country, country code, and a list of MBAs.
+        /// </summary>
+        /// <param name="country">The country for the new <see cref="MbaOptions"/> entity.</param>
+        /// <param name="countryCode">The country code for the new <see cref="MbaOptions"/> entity.</param>
+        /// <param name="mBas">The list of MBAs for the new <see cref="MbaOptions"/> entity.</param>
+        /// <returns>The newly created <see cref="MbaOptions"/> entity.</returns>
+        Task<MbaOptions> CreateMbaOptionsAsync(string country, string countryCode, IList<Mba> mBas);
+
+        /// <summary>
+        /// Asynchronously adds a range of <see cref="MbaOptions"/> entities to the database.
+        /// </summary>
+        /// <param name="mbAOptions">The list of <see cref="MbaOptions"/> entities to add.</param>
+        /// <returns>The list of added <see cref="MbaOptions"/> entities.</returns>
+        Task<IList<MbaOptions>> AddRangeMbaOptionsAsync(IList<MbaOptions> mbAOptions);
+
+        /// <summary>
+        /// Asynchronously deletes a specified <see cref="MbaOptions"/> entity from the database.
+        /// </summary>
+        /// <param name="mbAOptions">The <see cref="MbaOptions"/> entity to delete.</param>
+        Task DeleteMbaOptionsAsync(MbaOptions mbAOptions);
+
+        /// <summary>
+        /// Asynchronously deletes a range of <see cref="MbaOptions"/> entities from the database.
+        /// </summary>
+        /// <param name="mbAOptions">The list of <see cref="MbaOptions"/> entities to delete.</param>
+        Task DeleteRangeMbaOptionsAsync(IList<MbaOptions> mbAOptions);
+
+        /// <summary>
+        /// Asynchronously retrieves a <see cref="MbaOptions"/> entity by its ID.
+        /// </summary>
+        /// <param name="MBAOptionsId">The ID of the <see cref="MbaOptions"/> entity to retrieve.</param>
+        /// <returns>The <see cref="MbaOptions"/> entity with the specified ID.</returns>
+        Task<MbaOptions> GetMbaOptionsByIdAsync(Guid MBAOptionsId);
+
+        /// <summary>
+        /// Asynchronously retrieves all <see cref="MbaOptions"/> entities from the database.
+        /// </summary>
+        /// <returns>A list of all <see cref="MbaOptions"/> entities.</returns>
+        Task<IList<MbaOptions>> GetMbasOptionsAsync();
+
+        /// <summary>
+        /// Asynchronously updates a specified <see cref="MbaOptions"/> entity in the database.
+        /// </summary>
+        /// <param name="mbAOptions">The <see cref="MbaOptions"/> entity to update.</param>
+        Task UpdateMbaOptionsAsync(MbaOptions mbAOptions);
+
+        /// <summary>
+        /// Asynchronously updates a range of <see cref="MbaOptions"/> entities in the database.
+        /// </summary>
+        /// <param name="mbAOptions">The list of <see cref="MbaOptions"/> entities to update.</param>
+        Task UpdateRangeMbaOptionsAsync(IList<MbaOptions> mbAOptions);
+    }
+}
---
+++ Repository/Abstract/MBAbstract/IMbaRepository.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Entities;
+
+namespace Repository.Abstract.MBAbstract
+{
+    /// <summary>
+    /// Defines the interface for the Mba repository.
+    /// </summary>
+    public interface IMbaRepository : IRepository
+    {
+        /// <summary>
+        /// Asynchronously creates a new <see cref="Mba"/> entity with the specified code, name, and MbaOptionsId.
+        /// </summary>
+        /// <param name="code">The code for the new <see cref="Mba"/> entity.</param>
+        /// <param name="name">The name for the new <see cref="Mba"/> entity.</param>
+        /// <param name="MbaOptionsId">The MbaOptionsId for the new <see cref="Mba"/> entity.</param>
+        /// <returns>The newly created <see cref="Mba"/> entity.</returns>
+        Task<Mba> CreateMbAsync(string code, string name, Guid MbaOptionsId);
+
+        /// <summary>
+        /// Asynchronously adds a range of <see cref="Mba"/> entities to the database.
+        /// </summary>
+        /// <param name="mba">The list of <see cref="Mba"/> entities to add.</param>
+        /// <returns>The list of added <see cref="Mba"/> entities.</returns>
+        Task<IList<Mba>> AddRangeMbAsync(IList<Mba> mba);
+
+        /// <summary>
+        /// Asynchronously deletes a specified <see cref="Mba"/> entity from the database.
+        /// </summary>
+        /// <param name="mba">The <see cref="Mba"/> entity to delete.</param>
+        Task DeleteMbAsync(Mba mba);
+
+        /// <summary>
+        /// Asynchronously deletes a range of <see cref="Mba"/> entities from the database.
+        /// </summary>
+        /// <param name="mba">The list of <see cref="Mba"/> entities to delete.</param>
+        Task DeleteRangeMbAsync(IList<Mba> mba);
+
+        /// <summary>
+        /// Asynchronously retrieves a <see cref="Mba"/> entity by its ID.
+        /// </summary>
+        /// <param name="MbaId">The ID of the <see cref="Mba"/> entity to retrieve.</param>
+        /// <returns>The <see cref="Mba"/> entity with the specified ID.</returns>
+        Task<Mba> GetMbaByIdAsync(Guid MbaId);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of <see cref="Mba"/> entities.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="Mba"/> entities.</returns>
+        Task<IList<Mba>> GetAllMbaAsync();
+
+        /// <summary>
+        /// Asynchronously retrieves a list of <see cref="Mba"/> entities that are associated with the specified MbaOptions entity.
+        /// </summary>
+        /// <param name="mbaOptionsId">The ID of the MbaOptions entity to retrieve the associated <see cref="Mba"/> entities for.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a list of <see cref="Mba"/> entities associated with the specified <see cref="MbaOptions"/> entity.</returns>
+        Task<IList<Mba>> GetMbaByMbaOptionsAsync(Guid mbaOptionsId);
+
+        /// <summary>
+        /// Asynchronously updates a specified <see cref="Mba"/> entity in the database.
+        /// </summary>
+        /// <param name="mba">The <see cref="Mba"/> entity to update.</param>
+        Task UpdateMbAsync(Mba mba);
+
+        /// <summary>
+        /// Asynchronously updates a range of <see cref="Mba"/> entities in the database.
+        /// </summary>
+        /// <param name="mba">The list of <see cref="Mba"/> entities to update.</param>
+        Task UpdateRangeMbAsync(IList<Mba> mba);
+    }
+
+}
