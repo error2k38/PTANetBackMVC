4a402f586c379b650ce2038f9f8e92fba4ad664b
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 22:29:28 +0200

    Agregando Entidades. No se utilizan notaciones para estableces el tamaño y requisitos de las propiedades. De esta configuración se encargará en la capa de repositorio en el api-fluente. Se establece una relación de uno a Mucho entre las clases Mba y MbaOtions.

---
+++ Domain/Entities/Mba.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents the <see cref="Mba"/> entity.
+    /// </summary>
+    public class Mba
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the <see cref="Mba"/>.
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MbaId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code for the <see cref="Mba"/>.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the <see cref="Mba"/>.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the <see cref="MbaOptions"/> associated with the <see cref="Mba"/>.
+        /// </summary>
+        public Guid MbaOptionsId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="MbaOptions"/> associated with the <see cref="Mba"/>.
+        /// </summary>
+        public MbaOptions MbaOptions { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Mba"/> class.
+        /// This constructor is required by Entity Framework.
+        /// </summary>
+        public Mba() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Mba"/> class with the specified code and name.
+        /// </summary>
+        /// <param name="code">The code for the <see cref="Mba"/>.</param>
+        /// <param name="name">The name of the <see cref="Mba"/>.</param>
+        public Mba(string code, string name, Guid mbaOptionsId)
+        {
+            Code = code;
+            Name = name;
+            MbaOptionsId = mbaOptionsId;
+        }
+
+        #endregion Constructors
+    }
+
+}
---
+++ Domain/Entities/MbaOptions.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Represents the <see cref="MbaOptions"/> entity.
+    /// </summary>
+    public class MbaOptions
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the <see cref="MbaOptions"/>.
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MbaOptionsId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country for the <see cref="MbaOptions"/>.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country code for the <see cref="MbaOptions"/>.
+        /// </summary>
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of <see cref="Mba"/> entities associated with the <see cref="MbaOptions"/>.
+        /// </summary>
+        public IList<Mba> Mbas { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MbaOptions"/> class.
+        /// This constructor is required by Entity Framework.
+        /// </summary>
+        public MbaOptions()
+        {
+            Mbas = new List<Mba>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MbaOptions"/> class with the specified country and country code.
+        /// </summary>
+        /// <param name="country">The country for the <see cref="MbaOptions"/>.</param>
+        /// <param name="countryCode">The country code for the <see cref="MbaOptions"/>.</param>
+        public MbaOptions(string country, string countryCode)
+        {
+            Country = country;
+            CountryCode = countryCode;
+        }
+
+        #endregion Constructors
+    }
+
+}
