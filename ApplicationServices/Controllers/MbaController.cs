b84924265c6576b4cd334005e1c6d65b67531fba
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 23:51:18 +0200

    Se agregan los controladores con un cúmulo de servicios y operaciones básicas CRUD y filtrado por clave primaria. Se crea un endpoint para consumir el servicios deseado y almacenar los datos en la base de datos.

---
+++ ApplicationServices/Controllers/MbaController.cs
@@ -0,0 +1,248 @@
+﻿using Mappers.DTOs;
+using Mapper.Mappers;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Abstract.MBAbstract;
+
+namespace PTNetBackMVC.Controllers
+{
+    // Define the route and output type for the controller
+    [Route("api/[controller]"), Produces(typeof(JsonResult))]
+    [ApiController]
+    public class MbaController : ControllerBase
+    {
+        // Define a logger and a repository as private readonly fields
+        private readonly ILogger<MbaController> _logger;
+        private readonly IMbaRepository _mbaRepository;
+
+        // Initialize the logger and the repository in the constructor
+        public MbaController(ILogger<MbaController> logger, IMbaRepository mbaRepository)
+        {
+            _logger = logger;
+            _mbaRepository = mbaRepository;
+        }
+
+        #region GetActions
+
+        /// <summary>
+        /// Gets the MBAs by MBA options.
+        /// </summary>
+        /// <param name="id">The identifier of the MBA option.</param>
+        /// <returns>A <see cref="Task{ActionResult{IEnumerable{MbaDto}}}"/> object containing the asynchronous operation
+        /// that returns the list of MBAs of an associated option.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        [HttpGet(nameof(GetMbasByMbaOptions) + "/{id}")]
+        public async Task<ActionResult<IEnumerable<MbaDto>>> GetMbasByMbaOptions([FromRoute] Guid id)
+        {
+            // Begin a database transaction
+            _mbaRepository.BeginTransaction();
+
+            var mbas = await _mbaRepository.GetMbaByMbaOptionsAsync(id);
+
+            if (mbas is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(GetMbasByMbaOptions)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbasDto = mbas.Select(mba => mba.Map());
+
+            _mbaRepository.CommitTransaction();
+            return Ok(mbasDto);
+        }
+
+        /// <summary>
+        /// Gets the MBA by its identifier.
+        /// </summary>
+        /// <param name="id">The identifier of the MBA.</param>
+        /// <returns>A <see cref="Task{ActionResult{MbaDto}}}"/> object containing the asynchronous operation
+        /// that returns the MBA of the given identifier.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA could not be found.
+        [HttpGet(nameof(GetMbaById) + "/{id}")]
+        public async Task<ActionResult<MbaDto>> GetMbaById([FromRoute] Guid id)
+        {
+            // Begin a database transaction
+            _mbaRepository.BeginTransaction();
+
+            var mba = await _mbaRepository.GetMbaByIdAsync(id);
+
+            if (mba is null)
+            {
+                var msg = "Mbas not found";
+                _logger.LogError($" {nameof(GetMbaById)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaDto = mba.Map();
+            _mbaRepository.CommitTransaction();
+            return Ok(mbaDto);
+        }
+
+        #endregion GetActions
+
+        #region Post Actions
+
+        /// <summary>
+        /// Creates a new MBA.
+        /// </summary>
+        /// <param name="request">The request to create a new MBA.</param>
+        /// <returns>A <see cref="Task{ActionResult{MbaDto}}}"/> object containing the asynchronous operation
+        /// that returns the created MBA.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA could not be found.
+        [HttpPost(nameof(CreateMba))]
+        public async Task<ActionResult<MbaDto>> CreateMba([FromBody] MbaCreationDto request)
+        {
+            // Begin a database transaction
+            var _mbaOptionsRepository = (IMbaOptionsRepository)_mbaRepository;
+            _mbaRepository.BeginTransaction();
+
+            var AllMbas = await _mbaRepository.GetAllMbaAsync();
+
+            if (AllMbas.Any(mba => mba.Name.Equals(request.Name, StringComparison.OrdinalIgnoreCase)
+                || mba.Code.Equals(request.Code, StringComparison.OrdinalIgnoreCase)))
+            {
+                var msg = "There is already an entity with that data";
+                _logger.LogError($"{nameof(CreateMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaOptionsParent = await _mbaOptionsRepository.GetMbaOptionsByIdAsync(request.MbaOptionsId);
+
+            if (mbaOptionsParent is null)
+            {
+                var msg = "MbaOptions parent not found";
+                _logger.LogError($"{nameof(CreateMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mba = await _mbaRepository.CreateMbAsync(request.Code, request.Name, request.MbaOptionsId);
+
+            if (mba is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(CreateMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaDto = mba.Map();
+            _mbaRepository.CommitTransaction();
+            return Ok(mbaDto);
+        }
+
+        /// <summary>
+        /// Adds a range of MBAs.
+        /// </summary>
+        /// <returns>A <see cref="Task{ActionResult{IEnumerable{MbaDto}}}"/> object containing the asynchronous operation
+        /// that returns the list of added MBAs.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        [HttpPost(nameof(AddMbaByRange))]
+        public async Task<ActionResult<IEnumerable<MbaDto>>> AddMbaByRange()
+        {
+            // Begin a database transaction
+            _mbaRepository.BeginTransaction();
+
+            var mbaList = new List<Mba>();
+            mbaList = (await _mbaRepository.AddRangeMbAsync(mbaList)).ToList();
+
+            if (mbaList is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(AddMbaByRange)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaListDto = mbaList.Select(mba => mba.Map());
+            _mbaRepository.CommitTransaction();
+            return Ok(mbaListDto);
+        }
+
+        /// <summary>
+        /// Updates an existing MBA.
+        /// </summary>
+        /// <param name="request">The request to update an MBA.</param>
+        /// <returns>A <see cref="Task{ActionResult}"/> object containing the asynchronous operation.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA could not be found.
+        [HttpPost(nameof(UpdateMba))]
+        public async Task<ActionResult> UpdateMba([FromBody] MbaDto request)
+        {
+            // Begin a database transaction
+            _mbaRepository.BeginTransaction();
+
+            var oldMba = await _mbaRepository.GetMbaByIdAsync(request.MbaId);
+            if (oldMba is null)
+            {
+                var msg = "Mba not found";
+                _logger.LogError($"{nameof(UpdateMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var AllMbas = await _mbaRepository.GetAllMbaAsync();
+
+            if (AllMbas.Any(mba => mba.Name.Equals(request.Name, StringComparison.OrdinalIgnoreCase)
+                || mba.Code.Equals(request.Code, StringComparison.OrdinalIgnoreCase))
+                && !oldMba.Name.Equals(request.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                var msg = "There is already an entity with that data";
+                _logger.LogError($"{nameof(UpdateMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            oldMba.Update(request);
+            await _mbaRepository.UpdateMbAsync(oldMba);
+            _mbaRepository.CommitTransaction();
+
+            return Ok(new ResponseDTO { IsSuccessful = true });
+        }
+
+        #endregion Post Actions
+
+        #region Delete Actions
+
+        /// <summary>
+        /// Deletes an existing MBA.
+        /// </summary>
+        /// <param name="id">The identifier of the MBA to delete.</param>
+        /// <returns>A <see cref="Task{ActionResult}"/> object containing the asynchronous operation.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA could not be found.
+        [HttpDelete(nameof(DeleteMba) + "/{id}")]
+        public async Task<ActionResult> DeleteMba([FromRoute] Guid id)
+        {
+            // Begin a database transaction
+            _mbaRepository.BeginTransaction();
+
+            var mba = await _mbaRepository.GetMbaByIdAsync(id);
+            if (mba is null)
+            {
+                var msg = "Mba not found";
+                _logger.LogError($"{nameof(DeleteMba)} -> {msg}");
+                _mbaRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            await _mbaRepository.DeleteMbAsync(mba);
+            _mbaRepository.CommitTransaction();
+
+            return Ok(new ResponseDTO { IsSuccessful = true });
+        }
+
+        #endregion Delete Actions
+    }
+
+}
+
---
+++ ApplicationServices/Controllers/MbaOptionsController.cs
@@ -0,0 +1,262 @@
+﻿using Mappers.DTOs;
+using Mapper.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Abstract.MBAbstract;
+using System.Text.Json;
+
+namespace PTNetBackMVC.Controllers
+{
+    // Define the route and output type for the controller
+    [Route("api/[controller]"), Produces(typeof(JsonResult))]
+    [ApiController]
+    public class MbaOptionsController : ControllerBase
+    {
+        // Define a logger, a repository, and an HttpClient as private readonly fields
+        private readonly ILogger<MbaOptionsController> _logger;
+        private readonly IMbaOptionsRepository _mbaOptionsRepository;
+        private readonly HttpClient _httpClient;
+
+        // Initialize the logger, the repository, and the HttpClient in the constructor
+        public MbaOptionsController(ILogger<MbaOptionsController> logger, IMbaOptionsRepository mbaOptionsRepository, HttpClient httpClient)
+        {
+            _logger = logger;
+            _mbaOptionsRepository = mbaOptionsRepository;
+            _httpClient = httpClient;
+        }
+
+        #region GetActions
+
+        /// <summary>
+        /// Gets the MBA options.
+        /// </summary>
+        /// <returns>A <see cref="Task{ActionResult{IEnumerable{MbaOptionsDto}}}"/> object containing the asynchronous operation
+        /// that returns the list of MBA options.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        [HttpGet(nameof(GetMbasOptions))]
+        public async Task<ActionResult<IEnumerable<MbaOptionsDto>>> GetMbasOptions()
+        {
+            // Begin a database transaction
+            _mbaOptionsRepository.BeginTransaction();
+
+            var mbasOptions = await _mbaOptionsRepository.GetMbasOptionsAsync();
+
+            if (mbasOptions is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(GetMbasOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbasOptionsDto = mbasOptions.Select(mbaOptions => mbaOptions.Map());
+
+            _mbaOptionsRepository.CommitTransaction();
+            return Ok(mbasOptionsDto);
+        }
+
+        /// <summary>
+        /// Gets the MBA options by its identifier.
+        /// </summary>
+        /// <param name="id">The identifier of the MBA option.</param>
+        /// <returns>A <see cref="Task{ActionResult{MbaOptionsDto}}}"/> object containing the asynchronous operation
+        /// that returns the MBA options of the given identifier.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA options could not be found.
+        [HttpGet(nameof(GetMbaOptionsById) + "/{id}")]
+        public async Task<ActionResult<MbaOptionsDto>> GetMbaOptionsById([FromRoute] Guid id)
+        {
+            // Begin a database transaction
+            _mbaOptionsRepository.BeginTransaction();
+
+            var mbaOptions = await _mbaOptionsRepository.GetMbaOptionsByIdAsync(id);
+
+            if (mbaOptions is null)
+            {
+                var msg = "MbaOptions not found";
+                _logger.LogError($" {nameof(GetMbaOptionsById)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaOptionsDto = mbaOptions.Map();
+            _mbaOptionsRepository.CommitTransaction();
+            return Ok(mbaOptionsDto);
+        }
+
+        #endregion GetActions
+
+        #region Post Actions
+
+        /// <summary>
+        /// Creates a new MBA option.
+        /// </summary>
+        /// <param name="request">The request to create a new MBA option.</param>
+        /// <returns>A <see cref="Task{ActionResult{MbaOptionsDto}}}"/> object containing the asynchronous operation
+        /// that returns the created MBA option.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        [HttpPost(nameof(CreateMbaOptions))]
+        public async Task<ActionResult<MbaOptionsDto>> CreateMbaOptions([FromBody] MbaOptionsCreateDto request)
+        {
+            // Begin a database transaction
+            _mbaOptionsRepository.BeginTransaction();
+
+            var allMbaOptions = await _mbaOptionsRepository.GetMbasOptionsAsync();
+
+            if (allMbaOptions.Any(mbaOptions => mbaOptions.Country.Equals(request.Country, StringComparison.OrdinalIgnoreCase)
+                || mbaOptions.CountryCode.Equals(request.CountryCode, StringComparison.OrdinalIgnoreCase)))
+            {
+                var msg = "There is already an entity with that data";
+                _logger.LogError($"{nameof(CreateMbaOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaOtions = await _mbaOptionsRepository.CreateMbaOptionsAsync(request.Country, request.CountryCode);
+
+            if (mbaOtions is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(CreateMbaOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var mbaOptionsDto = mbaOtions.Map();
+            _mbaOptionsRepository.CommitTransaction();
+            return Ok(mbaOptionsDto);
+        }
+
+        /// <summary>
+        /// Adds a range of MBA options.
+        /// </summary>
+        /// <returns>A <see cref="Task{ActionResult{IEnumerable{MbaOptionsDto}}}"/> object containing the asynchronous operation
+        /// that returns the list of added MBA options.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status400BadRequest"/> if it could not be completed.
+        [HttpPost(nameof(AddMbaOptionsByRange))]
+        public async Task<ActionResult<IEnumerable<MbaOptionsDto>>> AddMbaOptionsByRange()
+        {
+            var response = await _httpClient.GetAsync("https://api.opendata.esett.com/EXP04/MBAOptions");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode);
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var mbaOptionsListDtos = JsonSerializer.Deserialize<List<MbaOptionsSerializableDto>>(content);
+
+            if (mbaOptionsListDtos is null)
+            {
+                var msg = "Has error occurred";
+                _logger.LogError($"{nameof(AddMbaOptionsByRange)} -> {msg}");
+            }
+
+            await AddMbaOptionsTodatabase(mbaOptionsListDtos);
+
+            _mbaOptionsRepository.CommitTransaction();
+            return Ok(mbaOptionsListDtos);
+        }
+
+        /// <summary>
+        /// Updates an existing MBA option.
+        /// </summary>
+        /// <param name="request">The request to update an MBA option.</param>
+        /// <returns>A <see cref="Task{ActionResult}"/> object containing the asynchronous operation.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA option could not be found.
+        [HttpPost(nameof(UpdateMbaOptions))]
+        public async Task<ActionResult> UpdateMbaOptions([FromBody] MbaOptionsDto request)
+        {
+            // Begin a database transaction
+            _mbaOptionsRepository.BeginTransaction();
+
+            var oldMbaOptions = await _mbaOptionsRepository.GetMbaOptionsByIdAsync(request.MbaOptionsId);
+            if (oldMbaOptions is null)
+            {
+                var msg = "Mba options not found";
+                _logger.LogError($"{nameof(UpdateMbaOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            var allMbaOptions = await _mbaOptionsRepository.GetMbasOptionsAsync();
+
+            if (allMbaOptions.Any(mbaOptions => mbaOptions.Country.Equals(request.Country, StringComparison.OrdinalIgnoreCase)
+               || mbaOptions.CountryCode.Equals(request.CountryCode, StringComparison.OrdinalIgnoreCase))
+                && !oldMbaOptions.Country.Equals(request.Country, StringComparison.OrdinalIgnoreCase))
+            {
+                var msg = "There is already an entity with that data";
+                _logger.LogError($"{nameof(UpdateMbaOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return BadRequest(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            oldMbaOptions.Update(request);
+            await _mbaOptionsRepository.UpdateMbaOptionsAsync(oldMbaOptions);
+            _mbaOptionsRepository.CommitTransaction();
+
+            return Ok(new ResponseDTO { IsSuccessful = true });
+        }
+
+        #endregion Post Actions
+
+        #region Delete Actions
+
+        /// <summary>
+        /// Deletes an existing MBA option.
+        /// </summary>
+        /// <param name="id">The identifier of the MBA option to delete.</param>
+        /// <returns>A <see cref="Task{ActionResult}"/> object containing the asynchronous operation.</returns>
+        /// <see cref="StatusCodes.Status200OK"/> if it was executed correctly.
+        /// <see cref="StatusCodes.Status404NotFound"/> if the MBA option could not be found.
+        [HttpDelete(nameof(DeleteMbaOptions) + "/{id}")]
+        public async Task<ActionResult> DeleteMbaOptions([FromRoute] Guid id)
+        {
+            // Begin a database transaction
+            _mbaOptionsRepository.BeginTransaction();
+
+            var mbaOptions = await _mbaOptionsRepository.GetMbaOptionsByIdAsync(id);
+            if (mbaOptions is null)
+            {
+                var msg = "Mba options not found";
+                _logger.LogError($"{nameof(DeleteMbaOptions)} -> {msg}");
+                _mbaOptionsRepository.RollbackTransaction();
+                return NotFound(new ResponseSingleErrorDTO { IsSuccessful = false, Error = msg });
+            }
+
+            await _mbaOptionsRepository.DeleteMbaOptionsAsync(mbaOptions);
+            _mbaOptionsRepository.CommitTransaction();
+
+            return Ok(new ResponseDTO { IsSuccessful = true });
+        }
+
+        #endregion Delete Actions
+
+        #region Helpers
+
+        /// <summary>
+        /// Adds a list of MBA options to the database.
+        /// </summary>
+        /// <param name="mbaOptionsListDtos">The list of MBA options to add.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        private async Task AddMbaOptionsTodatabase(List<MbaOptionsSerializableDto>? mbaOptionsListDtos)
+        {
+            _mbaOptionsRepository.BeginTransaction();
+            var _mbaRepository = (IMbaRepository)_mbaOptionsRepository;
+
+            foreach (var mbaOptionsDto in mbaOptionsListDtos)
+            {
+                var mbaOption = mbaOptionsDto.Map();
+                mbaOption = await _mbaOptionsRepository.CreateMbaOptionsAsync(mbaOption.Country, mbaOption.CountryCode);
+                _mbaOptionsRepository.PartialCommit();
+                var mbaOptionsDtos = mbaOptionsDto.Mbas.Select(mba => mba.Map(mbaOption.MbaOptionsId)).ToList();
+                await _mbaRepository.AddRangeMbAsync(mbaOptionsDtos);
+            }
+        }
+
+        #endregion Helpers
+    }
+}
