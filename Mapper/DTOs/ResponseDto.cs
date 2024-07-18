711adac1e9a91e2c8bc8e0a77a91042f5631c2bd
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 23:08:28 +0200

    Se agrega la clase dto base utilizada para mostrar el éxito o no de ciertas transacciones en la capa de servicios. Es usado en tipo Record en todos los dtos por su capacidad de ser inmutables y rapidez a la hora de ser serial izadas. Los dtos no son rastreados por la transacciones en la base de datos o de otro tipo y son recomendados en la operaciones de entrada y salida.

---
+++ Mapper/DTOs/ResponseDto.cs
@@ -0,0 +1,35 @@
+﻿namespace Mappers.DTOs
+{
+    /// <summary>
+    /// Represents a data transfer object (DTO) for responses.
+    /// </summary>
+    public record ResponseDTO
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the operation was successful.
+        /// </summary>
+        public bool IsSuccessful { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of error messages, if any.
+        /// </summary>
+        public IEnumerable<string>? Errors { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a data transfer object (DTO) for responses that contain a single error message.
+    /// </summary>
+    public record ResponseSingleErrorDTO
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the operation was successful.
+        /// </summary>
+        public bool IsSuccessful { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error message, if any.
+        /// </summary>
+        public string? Error { get; set; }
+    }
+
+}
