db05b214774605464e3a8fc45cc58268d78611c4
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 23:11:53 +0200

    Son agregadas los respectivos dtos relacionados con las clases entidad y sus operaciones de entrada y salida. Se usa el tipo record. Se utiliza herencia para evitar el uso reiterado de propiedades y más limpieza al código. Son usadas las notaciones para mantener un control y filtro los mínimos requisitos establecidos sobre las propiedades.

---
+++ Mapper/DTOs/MbaDto.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Mappers.DTOs
+{
+    /// <summary>
+    /// Base DTO for <see cref="Mba"/> entity.
+    /// </summary>
+    public record MbaBaseDto
+    {
+        /// <summary>
+        /// Code required for the <see cref="Mba"/> entity.
+        /// Must be between 5 and 16 characters.
+        /// </summary>
+        [Required(ErrorMessage = "Code is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The value must be between 1 and 50 characters long.")]
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Name required for the <see cref="Mba"/> entity.
+        /// Must be between 1 and 256 characters.
+        /// </summary>
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(256, MinimumLength = 1, ErrorMessage = "The value must be between 1 and 256 characters long.")]
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// DTO necessary for the creation of an <see cref="Mba"/> entity.
+    /// </summary>
+    public record MbaCreationDto : MbaBaseDto
+    {
+        /// <summary>
+        /// Mba Options ID required for the <see cref="MbaOptions"/> entity.
+        /// </summary>
+        [Required(ErrorMessage = "Mba Options ID is required")]
+        [JsonPropertyName("mbaOptionsId")]
+        public Guid MbaOptionsId { get; set; }
+    }
+
+    /// <summary>
+    /// DTO associated with the <see cref="Mba"/> entity.
+    /// </summary>
+    public record MbaDto : MbaCreationDto
+    {
+        /// <summary>
+        /// Mba ID required.
+        /// </summary>
+        [Required(ErrorMessage = "Mba ID is required.")]
+        [JsonPropertyName("mbaId")]
+        public Guid MbaId { get; set; }
+    }
+
+}
---
+++ Mapper/DTOs/MbaOptionsDto.cs
@@ -0,0 +1,60 @@
+﻿using Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Mappers.DTOs
+{
+    /// <summary>
+    /// Base DTO for MbaOptions entity.
+    /// </summary>
+    public record MbaOptionsBaseDto
+    {
+        /// <summary>
+        /// Country required for the MbaOptions entity.
+        /// Must be between 4 and 50 characters.
+        /// </summary>
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "The value must be between 4 and 50 characters long.")]
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// CountryCode required for the MbaOptions entity.
+        /// Must be between 5 and 16 characters.
+        /// </summary>
+        [Required(ErrorMessage = "CountryCode is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "The value must be between 1 and 50 characters long.")]
+        [JsonPropertyName("countryCode")]
+        public string CountryCode { get; set; }
+    }
+
+    /// <summary>
+    /// DTO necessary for the creation of an <see cref="MbaOptions"/> entity.
+    /// </summary>
+    public record MbaOptionsCreateDto : MbaOptionsBaseDto
+    {
+        // No additional properties
+    }
+
+    public record MbaOptionsDto : MbaOptionsBaseDto
+    {
+        /// <summary>
+        /// Required MbaOptions ID.
+        /// </summary>
+        [Required(ErrorMessage = "Mba options Id is required.")]
+        [JsonPropertyName("mbaOptionsId")]
+        public Guid MbaOptionsId { get; set; }
+    }
+
+    /// <summary>
+    /// Necessary data for communication with the external service.
+    /// </summary>
+    public record MbaOptionsSerializableDto : MbaOptionsBaseDto
+    {
+        /// <summary>
+        /// List of MbaDto. It is never null but can be empty.
+        /// </summary>
+        [JsonPropertyName("mbas")]
+        public IList<MbaBaseDto> Mbas { get; set; }
+    }
+}
