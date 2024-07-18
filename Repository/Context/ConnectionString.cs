e5d56752532ef8b6e8a6193c1232b3d0e28dd039
Adiel Alfonso <error2k38@gmail.com>
2024-07-18 22:36:50 +0200

    ConnectionString es la clase encargada en manejar la cadena de conexiones utilizada por el repositorio.

---
+++ Repository/Context/ConnectionString.cs
@@ -0,0 +1,38 @@
+﻿namespace Repository.Context
+{
+    /// <summary>
+    /// Models a database connection string.
+    /// </summary>
+    public abstract class ConnectionString
+    {
+        /// <summary>
+        /// Initializes a <see cref="ConnectionString"/> object.
+        /// </summary>
+        /// <param name="value">
+        /// Database connection string.
+        /// </param>
+        public ConnectionString(string value) => Value = value;
+
+        /// <summary>
+        /// Connection string.
+        /// </summary>
+        public string Value { get; }
+    }
+
+    /// <summary>
+    /// Models the connection string for the database.
+    /// </summary>
+    public class ConfigurationDBConnectionString : ConnectionString
+    {
+        /// <summary>
+        /// Initializes a <see cref="ConfigurationDBConnectionString"/> object.
+        /// </summary>
+        /// <param name="value">
+        /// Connection string to the database.
+        /// </param>
+        public ConfigurationDBConnectionString(string value) : base(value)
+        {
+        }
+    }
+
+}
